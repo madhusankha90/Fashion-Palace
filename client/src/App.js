405f4747f1a3b07c9dd327996411054b907fcdc0
madhusankha90 <madhusankha.networking@gmail.com>
2024-07-03 00:36:15 +0530

    error fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navigation from './customer/components/Navigation/Navigation'
 
 function App() {
   return (
     <div>
+      <Navigation/>
       <div className="container text-center text-cyan-400">this is new</div>
     </div>
   )
