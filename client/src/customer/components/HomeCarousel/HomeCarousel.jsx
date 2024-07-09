970115144149375a4382e93d1829f1e4f6b59232
madhusankha90 <madhusankha.networking@gmail.com>
2024-07-10 00:33:46 +0530

    HomeCarousel updated

--- client/src/customer/components/HomeCarousel/HomeCarousel.jsx
+++ client/src/customer/components/HomeCarousel/HomeCarousel.jsx
@@ -2,8 +2,8 @@ import React, { useState, useRef } from "react";
 import HomeCart from "../HomeCart/HomeCart";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
-import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
 const HomeCarousel = () => {
   const [activeIndex, setActiveIndex] = useState(0);
@@ -13,10 +13,12 @@ const HomeCarousel = () => {
     0: { items: 1 },
     568: { items: 2 },
     767: { items: 3 },
-    1024: { items: 5 },
+    1024: { items: 5.5 },
   };
 
-  const items = [1, 1, 1, 1, 1, 1, 1,1,1,1,1].map((item, index) => <HomeCart key={index} />);
+  const items = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((item, index) => (
+    <HomeCart key={index} />
+  ));
 
   const slidePrev = () => {
     if (carouselRef.current && activeIndex > 0) {
@@ -36,29 +38,35 @@ const HomeCarousel = () => {
 
   return (
     <div className="px-4 lg:px-8">
-      <div className="relative p-5 border">
+      <div className="relative p-5">
         <AliceCarousel
           ref={carouselRef}
           items={items}
           disableButtonsControls
+          disableDotsControls
           responsive={responsive}
           onSlideChanged={syncActiveIndex}
           activeIndex={activeIndex}
+          duration={2000}
         />
-        <button
-          onClick={slidePrev}
-          className="z-50 absolute top-1/2 left-0 transform -translate-y-1/2 bg-gray-200 py-2 rounded-sm hover:bg-gray-400 active:bg-gray-300"
-          aria-label="previous"
-        >
-          <KeyboardArrowLeftIcon />
-        </button>
-        <button
-          onClick={slideNext}
-          className="z-50 absolute top-1/2 right-0 transform -translate-y-1/2 bg-gray-200 py-2 rounded-sm hover:bg-gray-400 active:bg-gray-300"
-          aria-label="next"
-        >
-          <KeyboardArrowRightIcon />
-        </button>
+        {activeIndex !== 0 && (
+          <button
+            onClick={slidePrev}
+            className="z-50 absolute top-1/2 left-0 transform -translate-y-1/2 bg-gray-200 py-2 rounded-md hover:bg-gray-400 active:bg-gray-300"
+            aria-label="previous"
+          >
+            <KeyboardArrowLeftIcon />
+          </button>
+        )}
+        {activeIndex !== items.length - 5 && (
+          <button
+            onClick={slideNext}
+            className="z-50 absolute top-1/2 right-0 transform -translate-y-1/2 bg-gray-200 py-2 rounded-md hover:bg-gray-400 active:bg-gray-300"
+            aria-label="next"
+          >
+            <KeyboardArrowRightIcon />
+          </button>
+        )}
       </div>
     </div>
   );
--- client/src/customer/pages/HomePage.jsx
+++ client/src/customer/pages/HomePage.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
-import MainCarousel from '../../components/HomeCarousel/MainCarousel'
-import HomeCarousel from '../components/HomeCarousel/HomeCarousel'
 import Navigation from '../components/Navigation/Navigation'
+import HomeCarousel from '../components/HomeCarousel/HomeCarousel'
+import MainCrousel from '../components/MainCrousel/MainCrousel'
+
 
 
 const HomePage = () => {
   return (
     <div>
         <Navigation/>
-        <MainCarousel/>
+        <MainCrousel/>
 
-        <div>
+        <div className='space-y-10 py-20 flex flex-col justify-center'>
+            <HomeCarousel/>
+            <HomeCarousel/>
+            <HomeCarousel/>
+            <HomeCarousel/>
             <HomeCarousel/>
         </div>
     </div>
