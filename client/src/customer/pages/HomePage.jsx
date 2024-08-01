c4c7da1fef06203463c417c30a21c1ea9c13b584
madhusankha90 <madhusankha.networking@gmail.com>
2024-08-02 00:30:39 +0530

    add kids section

--- client/src/customer/pages/HomePage.jsx
+++ client/src/customer/pages/HomePage.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import HomeCarousel from '../components/HomeCarousel/HomeCarousel'
 import MainCrousel from '../components/MainCrousel/MainCrousel'
 import { womenClosthes } from '../../data/women.clothes'
-import { grey, yellow } from '@mui/material/colors'
+import { green, grey, yellow } from '@mui/material/colors'
 import { menClothes } from '../../data/mens.clothes'
+import { kids } from '../../data/kids'
 
 
 
@@ -16,9 +17,9 @@ const HomePage = () => {
         <div className='space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10'>
             <HomeCarousel data= {womenClosthes} sectionName={"SHOP FOR WOMEN"} textColor={grey[800]}/>
             <HomeCarousel data= {menClothes} sectionName={"SHOP FOR MEN"} textColor={grey[800]}/>
-            <HomeCarousel data= {womenClosthes} sectionName={"SHOP FOR KIDS"} textColor={grey[800]}/>
+            <HomeCarousel data= {kids} sectionName={"SHOP FOR KIDS"} textColor={grey[800]}/>
             <HomeCarousel data= {womenClosthes} sectionName={"SEASONAL COLLECTIONS"} textColor={grey[800]}/>
-            <HomeCarousel data= {womenClosthes} sectionName={"FLASH SALE"} textColor={yellow[500]}/>
+            <HomeCarousel data= {womenClosthes} sectionName={"FLASH SALE"} textColor={green[500]}/>
             
         </div>
     </div>
---
+++ client/src/data/kids.js
@@ -0,0 +1,64 @@
+import t1 from '../../src/assets/kids/T-shirts/t1.jpg'
+import s1 from '../../src/assets/kids/shorts/s1.jpg'
+import p1 from '../../src/assets/kids/pants/p1.jpg'
+import d1 from '../../src/assets/kids/dresses/d1.jpg'
+import n1 from '../../src/assets/kids/nightwear/n1.jpg'
+import sh1 from '../../src/assets/kids/shoes/sh1.jpg'
+import pi1 from '../../src/assets/kids/pillow/pi1.jpg'
+import h1 from '../../src/assets/kids/hats/h1.jpg'
+import pt1 from '../../src/assets/kids/plushtoys/pt1.jpg'
+import f1 from '../../src/assets/kids/footware/f1.jpg'
+
+
+export const kids = [
+    {
+        image: t1,
+        brand: 'GIRLS GRAPHIC PRINTED T-SHIRT',
+        title: "T-SHITRS"
+    },
+    {
+        image: s1,
+        brand: "ENZO BOYS CASUAL SHORTS",
+        title: "SHORTS"
+    },
+    {
+        image: p1,
+        brand: 'BOY CASUAL LONG PANTS',
+        title: "PANTS"
+    },
+    {
+        image: d1,
+        brand: 'AMY GIRLS PRINTED FROCK',
+        title: "DRESSES"
+    },
+    {
+        image: n1,
+        brand: 'KIDS LONG SLEEVE 2 PC PAJAMA SET',
+        title: "BOYS PYJAMA"
+    },
+    {
+        image: sh1,
+        brand: 'NEWBORN BABY BOOTIES',
+        title: "BABY CARE"
+    },
+    {
+        image: pi1,
+        brand: 'BACK SUPPORT SEAT CUSHION',
+        title: "PILLOW"
+    },
+    {
+        image: h1,
+        brand: 'KIDS GRAPHIC BASEBALL CAP',
+        title: "HATS & CAPS"
+    },
+    {
+        image: pt1,
+        brand: 'NEW CD BUNNY PLUSH TOY ',
+        title: "PLUSH TOYS"
+    },
+    {
+        image: f1,
+        brand: 'KIDS ONNEW PRINTED SLIPPER',
+        title: "FOOTWARE"
+    }
+];
